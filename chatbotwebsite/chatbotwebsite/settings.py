2fe4c51ded9416f9df8153dac4218a309c8b12dc
Henry Scheible <henry.scheible@gmail.com>
2018-11-24 14:59:44 -0600

    change production database to default

--- chatbotwebsite/chatbotwebsite/settings.py
+++ chatbotwebsite/chatbotwebsite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    'production': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
