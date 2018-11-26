e5d5148ca3758851629cc7a3c0cc9331069012e4
henryscheible <henry.scheible@gmail.com>
2018-11-26 16:42:07 +0000

    add django-heroku to settings.py

--- chatbotwebsite/chatbotwebsite/settings.py
+++ chatbotwebsite/chatbotwebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
