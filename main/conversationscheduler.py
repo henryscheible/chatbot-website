ea7aa2174698e51ce5a60aa1ee41b354e6671e51
henryscheible <henry.scheible@gmail.com>
2018-11-27 16:29:53 +0000

    write skeleton for conversationscheduler.py

--- main/conversationscheduler.py
+++ main/conversationscheduler.py
@@ -1,1 +1,15 @@
-class ConversationScheduler
+from main.models import *
+
+class ConversationScheduler:
+    
+    def setConversation(self, conversation):
+        self.conversation = conversation
+        
+    def initChatbot(self, chatbot):
+        self.chatbot = chatbot
+    
+    def userResponded(self, response)
+        
+    def endConversation(self)
+    
+
